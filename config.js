e87101d43e6d7e88d17971d9d02c940b3e0ce7f0
motoroadtripfrance-spec <motoroadtripfrance@gmail.com>
2026-04-29 17:33:12 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // 2. Ou garde config.example.js et remplace les valeurs ci-dessous.
 // 3. Ces deux valeurs se trouvent dans Supabase > Project Settings > API.
 
-window.MOTOROADTRIP_SUPABASE_URL = "https://tzghkxbxbsoaaunuiih.supabase.co";
+window.MOTOROADTRIP_SUPABASE_URL = "https://tzghkxbyxbsoaaunuuih.supabase.co";
 window.MOTOROADTRIP_SUPABASE_ANON_KEY = "sb_publishable_9jezbDJNBOFv5BhdVRhFMg_URQN7neJ";
